be82becd537c16a2208626cb67351a6439263950
G-NOBODY <WenbinTeng@163.com>
2021-03-03 21:36:33 +0800

    Update

---
+++ 125.h
@@ -0,0 +1,30 @@
+#include <string>
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        if (s.empty()) return true;
+
+        for (auto iter = s.begin(); iter != s.end();)
+        {
+            if (*iter >= '0' && *iter <= '9') {  iter++;               continue;}
+            if (*iter >= 'a' && *iter <= 'z') {  iter++;               continue;}
+            if (*iter >= 'A' && *iter <= 'Z') {*(iter++) += 'a' - 'A'; continue;}
+
+            iter = s.erase(iter);
+        }
+
+        return checkShrink(s, 0, s.size() - 1);
+    }
+
+private:
+    bool checkShrink(std::string& s, int i, int j)
+    {
+        while (i <= j)
+        {
+            if (s[i++] != s[j--]) return false;
+        }
+        
+        return true;
+    }
+};
