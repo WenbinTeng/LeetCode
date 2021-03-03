e07513015b2d64bb8bcc8ddfb545b3614af5a59b
G-NOBODY <WenbinTeng@163.com>
2021-03-03 19:36:04 +0800

    Update

---
+++ 104.h
@@ -0,0 +1,24 @@
+#include <algorithm>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return searchHeight(root, 0);
+    }
+
+private:
+    int searchHeight(TreeNode* node, int h)
+    {
+        return node == nullptr ? h : std::max(searchHeight(node->left, h + 1), searchHeight(node->right, h + 1));
+    }
+};
---
+++ 109.h
@@ -0,0 +1,45 @@
+#include <vector>
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        return head == nullptr ? nullptr : build(head, nullptr);
+    }
+
+private:
+    TreeNode* build(ListNode* head, ListNode* tail)
+    {
+        if (head == tail) return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != tail)
+        {
+            if (fast != tail) fast = fast->next; else break;
+            if (fast != tail) fast = fast->next; else break;
+            slow = slow->next;
+        }
+
+        return new TreeNode(slow->val, build(head, slow), build(slow->next, tail));
+    }
+};
