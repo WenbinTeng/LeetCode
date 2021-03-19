04e26909259f915464777023e508b8442c085a22
G-NOBODY <WenbinTeng@163.com>
2021-03-19 18:45:59 +0800

    Update

---
+++ 1544.h
@@ -0,0 +1,26 @@
+#include <string>
+
+class Solution {
+public:
+    std::string makeGood(std::string s) {
+        if (s.empty()) return "";
+
+        std::string res;
+        for (const auto& c : s)
+        {
+            if (!res.empty())
+            {
+                if (c >= 'A' && c <= 'Z' && c - res.back() == 'A' - 'a') {res.pop_back(); continue;}
+                if (c >= 'a' && c <= 'z' && c - res.back() == 'a' - 'A') {res.pop_back(); continue;}
+
+                res.push_back(c);
+            }
+            else
+            {
+                res.push_back(c);
+            }
+        }
+
+        return res;
+    }
+};
---
+++ 1603.h
@@ -0,0 +1,33 @@
+#include <vector>
+
+class ParkingSystem {
+public:
+    ParkingSystem(int big, int medium, int small) : big(big), medium(medium), small(small) {
+
+    }
+    
+    bool addCar(int carType) {
+        switch (carType)
+        {
+        case 1:
+            if (big    > 0) {--big;    return true;} else return false;
+        case 2:
+            if (medium > 0) {--medium; return true;} else return false;
+        case 3:
+            if (small  > 0) {--small;  return true;} else return false;
+        default:;
+        }
+
+        return false;
+    }
+
+    int big;
+    int medium;
+    int small;
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
