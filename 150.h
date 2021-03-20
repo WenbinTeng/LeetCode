6ab1b56d1055713195aeee6c6d3eee0412fdb3e2
git-ohhh <WenbinTeng@163.com>
2021-03-20 14:00:31 +0800

    Update

---
+++ 150.h
@@ -0,0 +1,34 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int evalRPN(std::vector<std::string>& tokens) {
+        std::vector<long long> nums;
+
+        for (const auto& token : tokens)
+        {
+            if (token.size() > 1 || token[0] >= '0' && token[0] <= '9')
+            {
+                nums.push_back(std::stoi(token));
+            }
+            else
+            {
+                long long b = nums.back();
+                nums.pop_back();
+                long long a = nums.back();
+                nums.pop_back();
+
+                nums.push_back(
+                    token[0] == '+' ? a + b :
+                    token[0] == '-' ? a - b :
+                    token[0] == '*' ? a * b :
+                    token[0] == '/' ? a / b :
+                    0
+                );
+            }
+        }
+
+        return nums.back();
+    }
+};
