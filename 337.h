61258524f879580daff7876bdf5dbf893e4be9e7
git-ohhh <WenbinTeng@163.com>
2021-03-25 13:06:05 +0800

    Update

---
+++ 337.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <unordered_map>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        dfs(root);
+        return std::max(f[root], g[root]);
+    }
+
+private:
+    std::unordered_map<TreeNode *, int> f, g;
+
+    void dfs(TreeNode *node)
+    {
+        if (node == nullptr) return;
+        dfs(node->left );
+        dfs(node->right);
+
+        f[node] = node->val + g[node->left] + g[node->right];
+        g[node] = std::max(f[node->left], g[node->left]) + std::max(f[node->right], g[node->right]);
+    }
+};
