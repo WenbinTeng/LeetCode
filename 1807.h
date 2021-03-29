301f1b8fff6ee49fe78fe517db19a830c75c1e6d
git-ohhh <WenbinTeng@163.com>
2021-03-29 15:26:23 +0800

    Update

---
+++ 1807.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::string evaluate(std::string s, std::vector<std::vector<std::string>>& knowledge) {
+        std::unordered_map<std::string, int> um;
+
+        for (int i = 0; i < knowledge.size(); ++i) um.insert({knowledge[i][0], i});
+
+        for (int i = 0; i < s.size();)
+        {
+            int l = s.find_first_of('(', i);
+            int r = s.find_first_of(')', i);
+
+            if (l != -1 && r != -1)
+            {
+                std::string index = s.substr(l + 1, r - l - 1);
+                std::string place = um.find(index) == um.end() ? "?" : knowledge[um[index]][1];
+                s.replace(l, r - l + 1, place);
+                i = (r + 1) + place.size() - (r - l + 1);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return s;
+    }
+};
---
+++ 190.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    unsigned int reverseBits(unsigned int n) {
+        std::string res;
+        for (int i = 0; i < 32; ++i)
+        {
+            res.push_back(n % 2 + '0');
+            n = n >> 1;
+        }
+
+        return std::stoul(res, 0, 2);
+    }
+};
