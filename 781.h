66f88aacf639a26f7363ff1e0a8a188c9f67f5b1
git-ohhh <WenbinTeng@163.com>
2021-04-04 13:35:27 +0800

    Update

---
+++ 781.h
@@ -0,0 +1,29 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    int numRabbits(std::vector<int>& answers) {
+        int res = 0;
+        std::unordered_map<int, int> um;
+
+        for (const auto& answer : answers)
+        {
+            if (um[answer] == answer + 1)
+            {
+                um[answer] = 1; res += answer + 1;
+            }
+            else
+            {
+                um[answer] += 1;
+            }
+        }
+
+        for (const auto& [answer, count] : um)
+        {
+            res += answer + 1;
+        }
+
+        return res;
+    }
+};
