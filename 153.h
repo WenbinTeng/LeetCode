5ef152e59beb48370119fb3ba72e3d9ca2b97f9e
git-ohhh <WenbinTeng@163.com>
2021-04-08 10:12:21 +0800

    Update

---
+++ 153.h
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Solution {
+public:
+    int findMin(std::vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r)
+        {
+            int mid = (l + r) / 2;
+
+            if (nums[mid] < nums[r])
+            {
+                r = mid;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        return nums[l];
+    }
+};
---
+++ 154.h
@@ -0,0 +1,29 @@
+#include <vector>
+
+class Solution {
+public:
+    int findMin(std::vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r)
+        {
+            int mid = (l + r) / 2;
+
+            if (nums[mid] < nums[r])
+            {
+                r = mid;
+            }
+            else if (nums[mid] > nums[r])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                --r;
+            }
+        }
+
+        return nums[l];
+    }
+};
