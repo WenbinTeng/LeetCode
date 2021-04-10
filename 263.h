85cfd0aaa420b5baaecc75e099894be0761b474e
git-ohhh <WenbinTeng@163.com>
2021-04-10 09:20:51 +0800

    Update

---
+++ 263.h
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n < 1) return false;
+
+        while (n && n % 2 == 0) n /= 2;
+        while (n && n % 3 == 0) n /= 3;
+        while (n && n % 5 == 0) n /= 5;
+
+        return n == 1;
+    }
+};
