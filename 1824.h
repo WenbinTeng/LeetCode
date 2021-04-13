ebcc4a69fdd0667a867214c741cfc2ef34797393
git-ohhh <WenbinTeng@163.com>
2021-04-13 10:00:23 +0800

    Update

---
+++ 1824.h
@@ -0,0 +1,32 @@
+#include <vector>
+
+class Solution {
+public:
+    int minSideJumps(std::vector<int>& obstacles) {
+        int n = obstacles.size();
+        std::vector<std::vector<int>> dp(3, std::vector<int>(n));
+
+        dp[0][0] = 1;
+        dp[1][0] = 0;
+        dp[2][0] = 1;
+
+        for (int i = 1; i < n; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                dp[j][i] = obstacles[i] - 1 == j ? MAX : std::min(
+                    dp[j][i - 1],
+                    std::min(
+                        dp[(j + 1) % 3][i - 1] + (obstacles[i] - 1 == (j + 1) % 3 ? MAX : 1),
+                        dp[(j + 2) % 3][i - 1] + (obstacles[i] - 1 == (j + 2) % 3 ? MAX : 1)
+                    )
+                );
+            }
+        }
+
+        return std::min(dp[0].back(), std::min(dp[1].back(), dp[2].back()));
+    }
+
+private:
+    int MAX = 0x3f3f3f3f;
+};
---
+++ 783.h
@@ -0,0 +1,35 @@
+#include <algorithm>
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        inorder(root);
+        return res;
+    }
+
+private:
+    int val = 0x3f3f3f3f;
+    int res = 0x3f3f3f3f;
+
+    void inorder(TreeNode* node)
+    {
+        if (node == nullptr) return;
+
+        inorder(node->left);
+
+        res = std::min(res, std::abs(node->val - val));
+        val = node->val;
+
+        inorder(node->right);
+    }
+};
