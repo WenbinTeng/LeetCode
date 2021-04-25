acfb3f4bf128f1417a8ea3a4634bc2e9452f0e1c
git-ohhh <WenbinTeng@163.com>
2021-04-25 10:23:21 +0800

    Update

---
+++ 897.h
@@ -0,0 +1,31 @@
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        head = tail = new TreeNode();
+        inorder(root);
+        return head->right;
+    }
+
+private:
+    TreeNode* head;
+    TreeNode* tail;
+
+    void inorder(TreeNode* node)
+    {
+        if (node == nullptr) return;
+
+        inorder(node->left);
+        tail->right = new TreeNode(node->val);
+        tail = tail->right;
+        inorder(node->right);
+    }
+};
