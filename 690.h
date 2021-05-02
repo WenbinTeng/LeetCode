50123fd5ac9f0f78c8df6c5fd6f64f24491af2d1
git-ohhh <WenbinTeng@163.com>
2021-05-02 09:46:27 +0800

    Update

---
+++ 690.h
@@ -0,0 +1,42 @@
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+class Employee
+{
+public:
+    int id;
+    int importance;
+    std::vector<int> subordinates;
+};
+
+class Solution {
+public:
+    int getImportance(std::vector<Employee*> employees, int id) {
+        int res = 0;
+        std::unordered_map<int, Employee*> um;
+        std::queue<int> q;
+
+        for (const auto& employee : employees)
+        {
+            um.insert({employee->id, employee});
+        }
+
+        q.push(id);
+
+        while (!q.empty())
+        {
+            int currId = q.front();
+            
+            for (const auto& subordinate : um[currId]->subordinates)
+            {
+                q.push(subordinate);
+            }
+            q.pop();
+
+            res += um[currId]->importance;
+        }
+
+        return res;
+    }
+};
