10929e17433e079b1437a3f3741794c620476092
git-ohhh <WenbinTeng@163.com>
2021-05-07 10:24:23 +0800

    Update

---
+++ 1486.h
@@ -0,0 +1,12 @@
+#include <vector>
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int res = 0;
+        
+        for (int i = 0; i < n; ++i) res = res ^ (start + 2 * i);
+
+        return res;
+    }
+};
