c99961fbfcc2e5d4aee1e872e5ca151d674373a3
git-ohhh <WenbinTeng@163.com>
2021-05-18 12:51:28 +0800

    Update

---
+++ 1863.h
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution {
+public:
+    int subsetXORSum(std::vector<int>& nums) {
+        dfs(nums, 0, 0);
+        return res;
+    }
+
+private:
+    int res = 0;
+
+    void dfs(std::vector<int>& nums, int x, int index)
+    {
+        if (index == nums.size()) {res += x; return;}
+
+        dfs(nums, x, index + 1);
+        x = x ^ nums[index];
+        dfs(nums, x, index + 1);
+        x = x ^ nums[index];
+    }
+};
---
+++ 1866.h
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution {
+public:
+    int rearrangeSticks(int n, int k) {
+        std::vector<int> f(k + 1);
+        std::vector<int> g(k + 1);
+
+        f[0] = 1;
+
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= k; ++j)
+            {
+                g[j] = ((long long)f[j] * (i - 1) + f[j - 1]) % (int)(1e+9+7);
+            }
+            f = std::move(g);
+            g = std::vector<int>(k + 1);
+        }
+
+        return f[k];
+    }
+};
