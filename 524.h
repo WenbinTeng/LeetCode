1ac72d3e922cd09779004093482a24ff47c8de8e
WenbinTeng <WenbinTeng@163.com>
2021-09-14 10:33:03 +0800

    update

---
+++ 524.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    std::string findLongestWord(std::string s, std::vector<std::string>& dictionary) {
+        std::sort(dictionary.begin(), dictionary.end(), [](std::string& s1, std::string& s2){ return s1.size() == s2.size() ? s1 < s2 : s1.size() > s2.size(); });
+
+        for (const auto word : dictionary)
+        {
+            if (isMatch(s, word)) return word;
+        }
+
+        return "";
+    }
+
+private:
+    bool isMatch(const std::string& s, const std::string& w)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.size() && j < w.size())
+        {
+            if (s[i] == w[j]) ++j;
+            ++i;
+        }
+
+        return j == w.size();
+    }
+};
