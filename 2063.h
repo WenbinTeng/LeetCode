7d62cacb9479791d5f1fb210970e0d768603c117
WenbinTeng <WenbinTeng@163.com>
2021-11-14 10:12:06 +0800

    update

---
+++ 2063.h
@@ -0,0 +1,26 @@
+#include <string>
+
+class Solution {
+public:
+    long long countVowels(std::string word) {
+        long long res = 0;
+
+        for (int i = 0; i < word.size(); ++i)
+        {
+            switch (word[i])
+            {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                res += (i + 1) * (word.size() - i);
+                break;
+            default:
+                break;
+            }
+        }
+
+        return res;
+    }
+};
