54b72c665223c0417fcf60cbc5d697309401326e
WenbinTeng <WenbinTeng@163.com>
2022-01-02 10:27:38 +0800

    update

---
+++ 2119.h
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        return num == 0 ? true : num % 10 != 0;
+    }
+};
---
+++ 2121.h
@@ -0,0 +1,32 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<long long> getDistances(std::vector<int>& arr) {
+        std::vector<long long> res(arr.size());
+        std::unordered_map<int, std::vector<int>> indexMap;
+
+        for (int i = 0; i < arr.size(); ++i)
+        {
+            indexMap[arr[i]].push_back(i);
+        }
+
+        for (const auto& [key, indexes] : indexMap)
+        {
+            for (int i = 1; i < indexes.size(); ++i)
+            {
+                res[indexes[0]] += indexes[i] - indexes[0];
+            }
+            for (int i = 1; i < indexes.size(); ++i)
+            {
+                long long diff = indexes[i] - indexes[i - 1];
+                long long prev = i - 1;
+                long long next = indexes.size() - 1 - i;
+                res[indexes[i]] = res[indexes[i - 1]] + prev * diff - next * diff;
+            }
+        }
+
+        return res;
+    }
+};
