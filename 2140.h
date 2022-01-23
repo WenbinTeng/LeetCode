b0a7f23e406c7588581a3117827a50ef88eef5ff
WenbinTeng <WenbinTeng@163.com>
2022-01-23 10:10:32 +0800

    update

---
+++ 2140.h
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution {
+public:
+    long long mostPoints(std::vector<std::vector<int>>& questions) {
+        long long res = 0;
+        std::vector<long long> dp(questions.size() + 1);
+
+        for (int i = 0; i < questions.size(); ++i)
+        {
+            dp[i + 1] = std::max(dp[i], dp[i + 1]);
+            int next = std::min(i + 1 + questions[i][1], (int)questions.size());
+            dp[next] = std::max(dp[next], dp[i] + questions[i][0]);
+        }
+
+        return dp[questions.size()];
+    }
+};
