f77b3291ea649e47100df73c65e774db1a568cd0
WenbinTeng <WenbinTeng@163.com>
2022-03-13 10:25:29 +0800

    update

---
+++ 2194.h
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    std::vector<std::string> cellsInRange(std::string s) {
+        std::vector<std::string> res;
+        char col1 = s[0];
+        char row1 = s[1];
+        char col2 = s[3];
+        char row2 = s[4];
+
+        for (char i = col1; i <= col2; ++i)
+        {
+            for (char j = row1; j <= row2; ++j)
+            {
+                std::string t = "";
+                t.push_back(i);
+                t.push_back(j);
+                res.push_back(t);
+            }
+        }
+
+        return res;
+    }
+};
---
+++ 2197.h
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<int> replaceNonCoprimes(std::vector<int>& nums) {
+        std::vector<int> res;
+
+        for (const auto num : nums)
+        {
+            res.push_back(num);
+
+            int n = res.size();
+            int t = n >= 2 ? std::__gcd(res[n - 2], res[n - 1]) : 0;
+
+            while (t > 1)
+            {
+                res[n - 2] *= res[n - 1] / t;
+                res.pop_back();
+                --n;
+                t = n >= 2 ? std::__gcd(res[n - 2], res[n - 1]) : 0;
+            }
+        }
+
+        return res;
+    }
+};
