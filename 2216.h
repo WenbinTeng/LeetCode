325696cdf2e123005fa699d310175262f04d39b5
WenbinTeng <WenbinTeng@163.com>
2022-04-03 10:30:23 +0800

    update

---
+++ 2216.h
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution {
+public:
+    int minDeletion(std::vector<int>& nums) {
+        int res = 0;
+
+        int i;
+        int j;
+        for (i = 0; i < nums.size(); i += 2)
+        {
+            for (j = i + 1; j < nums.size() && nums[i] == nums[j]; ++j);
+            res += j - i - 1;
+            i = j - 1;
+        }
+
+        if ((nums.size() - res) % 2 == 1) ++res;
+
+        return res;
+    }
+};
