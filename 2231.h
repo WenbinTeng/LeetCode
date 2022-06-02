76998b520c06e578a25ca02d543f5af4db380f7c
WenbinTeng <WenbinTeng@163.com>
2022-06-02 09:49:45 +0800

    update

---
+++ 2231.h
@@ -0,0 +1,55 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int largestInteger(int num) {
+        int res = 0;
+        std::vector<int> digits;
+        std::vector<int> odd;
+        std::vector<int> even;
+
+        while (num)
+        {
+            int digit = num % 10;
+
+            digits.push_back(digit);
+
+            if (digit & 1)
+            {
+                even.push_back(digit);
+            }
+            else
+            {
+                odd.push_back(digit);
+            }
+
+            num /= 10;
+        }
+
+        std::sort(odd.begin(), odd.end(), std::greater<int>());
+        std::sort(even.begin(), even.end(), std::greater<int>());
+        
+        for (int i = 0; i < digits.size(); ++i)
+        {
+            if (digits[i] & 1)
+            {
+                digits[i] = even.back();
+                even.pop_back();
+            }
+            else
+            {
+                digits[i] = odd.back();
+                odd.pop_back();
+            }
+        }
+        
+        while (!digits.empty())
+        {
+            res = res * 10 + digits.back();
+            digits.pop_back();
+        }
+
+        return res;
+    }
+};
---
+++ 2234.h
@@ -0,0 +1,56 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    long long maximumBeauty(std::vector<int>& flowers, long long newFlowers, int target, int full, int partial) {
+        long long res = 0;
+        std::vector<long long> prevSum(flowers.size() + 1);
+
+        std::sort(flowers.begin(), flowers.end());
+
+        if (flowers.front() >= target) return flowers.size() * (long long)full;
+
+        for (int i = 0; i < flowers.size(); ++i)
+        {
+            prevSum[i + 1] = prevSum[i] + flowers[i];
+        }
+
+        for (int i = flowers.size() - 1; i >= 0; --i)
+        {
+            if (flowers[i] >= target) continue;
+
+            int l = flowers[0];
+            int r = target - 1;
+
+            while (l <= r)
+            {
+                int mid = (l + r) / 2;
+                
+                if (check(flowers, newFlowers, prevSum, mid, i + 1))
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+
+            res = std::max(res, (long long)(flowers.size() - i - 1) * full + (long long)(l - 1) * partial);
+            newFlowers -= target - flowers[i];
+            if (newFlowers <= 0) return res;
+        }
+
+        if (newFlowers > 0) res = std::max(res, (long long)flowers.size() * full);
+
+        return res;
+    }
+
+private:
+    bool check(std::vector<int>& flowers, long long newFlowers, std::vector<long long>& prevSum, int value, int index)
+    {
+        long long p = std::lower_bound(flowers.begin(), flowers.begin() + index, value) - flowers.begin();
+        return newFlowers >= p * value - prevSum[p];
+    }
+};
