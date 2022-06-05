2ffca86e4a64f4bd8fc84643eeaf83c42539aa85
WenbinTeng <WenbinTeng@163.com>
2022-06-05 10:24:37 +0800

    update

---
+++ 2287.h
@@ -0,0 +1,40 @@
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    int rearrangeCharacters(std::string s, std::string target) {
+        int res = 0;
+        std::unordered_map<char, int> um1;
+        std::unordered_map<char, int> um2;
+
+        for (const auto c : s)
+        {
+            ++um1[c];
+        }
+
+        for (const auto c : target)
+        {
+            ++um2[c];
+        }
+
+        while (true)
+        {
+            for (auto [c, cnt] : um2)
+            {
+                if (um1[c] >= cnt)
+                {
+                    um1[c] -= cnt;
+                }
+                else
+                {
+                    return res;
+                }
+            }
+
+            ++res;
+        }
+
+        return -1;
+    }
+};
---
+++ 2289.h
@@ -0,0 +1,27 @@
+#include <vector>
+#include <stack>
+
+class Solution {
+public:
+    int totalSteps(std::vector<int>& nums) {
+        int res = 0;
+        std::stack<std::pair<int, int>> s;
+
+        for (const auto num : nums)
+        {
+            int maxT = 0;
+
+            while (!s.empty() && s.top().first <= num)
+            {
+                maxT = std::max(maxT, s.top().second);
+                s.pop();
+            }
+            
+            if (!s.empty()) ++maxT;
+            res = std::max(res, maxT);
+            s.push({num, maxT});
+        }
+
+        return res;
+    }
+};
