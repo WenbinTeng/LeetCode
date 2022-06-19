bca75928876374c88232099ac4cd6b6e3b34e2d3
WenbinTeng <WenbinTeng@163.com>
2022-06-19 10:24:06 +0800

    update

---
+++ 2303.h
@@ -0,0 +1,32 @@
+#include <vector>
+
+class Solution {
+public:
+    double calculateTax(std::vector<std::vector<int>>& brackets, int income) {
+        double res = 0;
+
+        if (income <= brackets[0][0])
+        {
+            return (double)income * brackets[0][1] / 100.0;
+        }
+        else
+        {
+            res += (double)brackets[0][0] * brackets[0][1] / 100.0;
+        }
+
+        for (int i = 1; i < brackets.size(); ++i)
+        {
+            if (income > brackets[i][0])
+            {
+                res += (double)(brackets[i][0] - brackets[i - 1][0]) * brackets[i][1] / 100.0;
+            }
+            else
+            {
+                res += (double)(income - brackets[i - 1][0]) * brackets[i][1] / 100.0;
+                break;
+            }
+        }
+
+        return res;
+    }
+};
---
+++ 2304.h
@@ -0,0 +1,34 @@
+#include <vector>
+
+class Solution {
+public:
+    int minPathCost(std::vector<std::vector<int>>& grid, std::vector<std::vector<int>>& moveCost) {
+        const int inf = 0x3f3f3f3f;
+        int res = inf;
+        int m = grid.size();
+        int n = grid[0].size();
+        std::vector<std::vector<int>> cost(grid);
+
+        for (int i = 1; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                int c = inf;
+
+                for (int k = 0; k < grid[0].size(); ++k)
+                {
+                    c = std::min(c, cost[i - 1][k] + moveCost[grid[i - 1][k]][j]);
+                }
+
+                cost[i][j] += c;
+            }
+        }
+
+        for (int i = 0; i < n; ++i)
+        {
+            res = std::min(res, cost[m - 1][i]);
+        }
+
+        return res;
+    }
+};
