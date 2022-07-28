1228f1f49085575fb0992205dea88033ac9213c7
wenbinteng <wenbinteng@163.com>
2022-07-28 20:57:58 +0800

    update

---
+++ 2335.h
@@ -0,0 +1,37 @@
+#include <vector>
+#include <queue>
+
+class Solution {
+public:
+    int fillCups(std::vector<int>& amount) {
+        int res = 0;
+        std::priority_queue<int> q;
+
+        if (amount[0] > 0) q.push(amount[0]);
+        if (amount[1] > 0) q.push(amount[1]);
+        if (amount[2] > 0) q.push(amount[2]);
+
+        while (!q.empty())
+        {
+            if (q.size() >= 2)
+            {
+                int a = q.top();
+                q.pop();
+                int b = q.top();
+                q.pop();
+                
+                if (--a > 0) q.push(a);
+                if (--b > 0) q.push(b);
+
+                ++res;
+            }
+            else
+            {
+                res += q.top();
+                break;
+            }
+        }
+
+        return res;
+    }
+};
---
+++ 2337.h
@@ -0,0 +1,24 @@
+#include <string>
+#include <stack>
+#include <algorithm>
+
+class Solution {
+public:
+    bool canChange(std::string &start, std::string &target) {
+        std::string s = start;
+        std::string t = target;
+
+        s.erase(std::remove(s.begin(), s.end(), '_'), s.end());
+        t.erase(std::remove(t.begin(), t.end(), '_'), t.end());
+        if (s != t) return false;
+
+        for (int i = 0, j = 0; i < start.length(); ++i) {
+            if (start[i] == '_') continue;
+            while (target[j] == '_') ++j;
+            if (i != j && (start[i] == 'L') == (i < j)) return false;
+            ++j;
+        }
+        
+        return true;
+    }
+};
