d9d42ea8205443ce83f0b5b6250f395c9a9d37f4
WenbinTeng <WenbinTeng@163.com>
2023-01-02 11:33:21 +0800

    update

---
+++ 1961.h
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    bool isPrefixString(std::string s, std::vector<std::string> &words) {
+        int index = 0;
+
+        for (const auto word : words) {
+            for (const auto c : word) {
+                if (index == s.size()) {
+                    return false;
+                }
+                if (c == s[index]) {
+                    ++index;
+                } else {
+                    return false;
+                }
+            }
+            if (index == s.size()) {
+                return true;
+            }
+        }
+
+        return index == s.size();
+    }
+};
