2d42d7ed4af297e21e381847bcabc971e82d6084
WenbinTeng <WenbinTeng@163.com>
2023-01-03 21:16:31 +0800

    update

---
+++ 139.h
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool wordBreak(std::string s, std::vector<std::string> &wordDict) {
+        auto dp = std::vector<bool>(s.size() + 1);
+        auto wordDictSet = std::unordered_set<std::string>();
+        
+        for (const auto word : wordDict) {
+            wordDictSet.insert(word);
+        }
+
+        dp[0] = true;
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && wordDictSet.find(s.substr(j, i - j)) != wordDictSet.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.size()];
+    }
+};
