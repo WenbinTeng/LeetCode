4961838c541588bd3bcc4152fafc8d7405e0cb65
WenbinTeng <WenbinTeng@163.com>
2023-01-21 00:55:29 +0800

    update

---
+++ 134.h
@@ -0,0 +1,14 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+class Solution {
+public:
+    int canCompleteCircuit(std::vector<int> &gas, std::vector<int> &cost) {
+        gas[0] -= cost[0];
+        for (int i = 1; i < gas.size(); ++i) {
+            gas[i] = gas[i] - cost[i] + gas[i - 1];
+        }
+        return gas.back() < 0 ? -1 : (std::min_element(gas.begin(), gas.end()) - gas.begin() + 1) % gas.size();
+    }
+};
