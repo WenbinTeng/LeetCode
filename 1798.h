ba2a8537f249be8685d26c54228ce39136d50149
WenbinTeng <WenbinTeng@163.com>
2023-02-04 09:32:11 +0800

    update

---
+++ 1798.h
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int getMaximumConsecutive(std::vector<int>& coins) {
+        int res = 1;
+
+        std::sort(coins.begin(), coins.end());
+
+        for (const auto coin : coins) {
+            if (coin > res)
+                break;
+            res += coin;
+        }
+
+        return res;
+    }
+};
