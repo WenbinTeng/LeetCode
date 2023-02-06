b8444177f2e9cc50be08acb738c27cc6c9262494
WenbinTeng <WenbinTeng@163.com>
2023-02-06 11:19:11 +0800

    update

---
+++ 2331.h
@@ -0,0 +1,25 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool evaluateTree(TreeNode *root) {
+        if (root->left == nullptr || root->right == nullptr) {
+            if (root->val == 0)
+                return false;
+            else
+                return true;
+        } else {
+            if (root->val == 2)
+                return evaluateTree(root->left) | evaluateTree(root->right);
+            else
+                return evaluateTree(root->left) & evaluateTree(root->right);
+        }
+    }
+};
