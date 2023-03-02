275ba64d1ade910ef701e40ac12b571e4d5ef4c5
WenbinTeng <WenbinTeng@163.com>
2023-03-02 10:22:56 +0800

    update

---
+++ 883.h
@@ -0,0 +1,26 @@
+#include <vector>
+
+class Solution {
+public:
+    int projectionArea(std::vector<std::vector<int>> &grid) {
+        int xyArea = 0;
+        int yzArea = 0;
+        int zxArea = 0;
+
+        for (int i = 0; i < grid.size(); i++) {
+            int yzHeight = 0;
+            int zxHeight = 0;
+
+            for (int j = 0; j < grid.size(); j++) {
+                xyArea += grid[i][j] > 0 ? 1 : 0;
+                yzHeight = std::max(yzHeight, grid[j][i]);
+                zxHeight = std::max(zxHeight, grid[i][j]);
+            }
+
+            yzArea += yzHeight;
+            zxArea += zxHeight;
+        }
+
+        return xyArea + yzArea + zxArea;
+    }
+};
