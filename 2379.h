47be916a3d23c3368c65801d1996f606d5034b19
WenbinTeng <WenbinTeng@163.com>
2023-03-09 00:00:00 +0800

    update

---
+++ 2379.h
@@ -0,0 +1,28 @@
+#include <string>
+
+class Solution {
+public:
+    int minimumRecolors(std::string blocks, int k) {
+        int res = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < k; ++i) {
+            if (blocks[i] == 'W') {
+                ++cnt;
+            }
+        }
+
+        res = cnt;
+        for (int i = k; i < blocks.size(); ++i) {
+            if (blocks[i - k] == 'W' && blocks[i] == 'B') {
+                --cnt;
+            }
+            if (blocks[i - k] == 'B' && blocks[i] == 'W') {
+                ++cnt;
+            }
+            res = std::min(res, cnt);
+        }
+
+        return res;
+    }
+};
