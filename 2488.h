e73708157769e17781207b9e0bb74ed9bc90ec7e
WenbinTeng <WenbinTeng@163.com>
2023-03-16 10:25:30 +0800

    update

---
+++ 2488.h
@@ -0,0 +1,25 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    int countSubarrays(std::vector<int> &nums, int k) {
+        int res = 0;
+        int pos = std::find(nums.begin(), nums.end(), k) - nums.begin();
+        std::unordered_map<int, int> cnt;
+        
+        cnt[0] = 1;
+        for (int i = pos - 1, x = 0; i >= 0; --i) {
+            x += nums[i] < k ? 1 : -1;
+            ++cnt[x];
+        }
+
+        res = cnt[0] + cnt[-1];
+        for (int i = pos + 1, x = 0; i < nums.size(); ++i) {
+            x += nums[i] > k ? 1 : -1;
+            res += cnt[x] + cnt[x - 1];
+        }
+
+        return res;
+    }
+};
