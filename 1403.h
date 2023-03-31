3569c3077e17ca0d74266cf2c15f6cfc23f43b68
wenbinteng <wenbinteng@163.com>
2023-03-31 15:17:21 +0800

    update

---
+++ 1403.h
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <queue>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> minSubsequence(std::vector<int> &nums) {
+        int sum = 0;
+        std::vector<int> res;
+        std::priority_queue<int, std::vector<int>, std::less<int>> pq;
+
+        for (const auto num : nums) {
+            sum += num;
+            pq.push(num);
+        }
+
+        sum = std::floor(sum / 2.0);
+
+        while (!pq.empty() && sum >= 0) {
+            res.push_back(pq.top());
+            sum -= pq.top();
+            pq.pop();
+        }
+
+        return res;
+    }
+};
