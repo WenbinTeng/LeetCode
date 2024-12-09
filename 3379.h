1e052b9dd96411889c33a40922642580bf32501c
wenbinteng <wenbinteng@163.com>
2024-12-09 16:08:01 +0800

    update

---
+++ 3379.h
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> constructTransformedArray(std::vector<int>& nums) {
+        std::vector<int> res;
+        int n = nums.size();
+
+        for (int i = 0; i < nums.size(); i++) {
+            int step = (i + nums[i] + n) % n;
+            while (step < 0)
+                step = (step + n) % n;
+            res.push_back(nums[step]);
+        }
+
+        return res;
+    }
+};
