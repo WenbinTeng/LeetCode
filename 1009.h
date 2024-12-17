8bac3b5f2c0494ea82c45f343b16245117b5b2a3
wenbinteng <wenbinteng@163.com>
2024-12-17 16:31:52 +0800

    update

---
+++ 1009.h
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+
+        for (int i = 0; i < 32; i++) {
+            int N = 1 << i;
+            if (N > n) {
+                return N - 1 - n;
+            }
+        }
+
+        return 0;
+    }
+};
