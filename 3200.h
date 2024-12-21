d6081bbd1068ce9b8e98994a0257fdb43824806f
wenbinteng <wenbinteng@163.com>
2024-12-21 17:36:27 +0800

    update

---
+++ 3200.h
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Solution {
+public:
+    int maxHeightOfTriangle(int red, int blue) {
+        auto check = [&](int oddCnt, int evenCnt) -> int {
+            for (int i = 1; i <= 20; i++) {
+                if (i % 2 == 0) {
+                    if (oddCnt < i)
+                        return i - 1;
+                    else
+                        oddCnt -= i;
+                } else {
+                    if (evenCnt < i)
+                        return i - 1;
+                    else
+                        evenCnt -= i;
+                }
+            }
+            return 0;
+        };
+
+        return std::max(check(red, blue), check(blue, red));
+    }
+};
