31c9b3fd95acfe60dc4267ececaa6d8baa988d20
wenbinteng <wenbinteng@163.com>
2024-12-25 17:14:33 +0800

    update

--- 3264.h
+++ 3264.h
@@ -4,24 +4,27 @@
 class Solution {
 public:
     std::vector<int> getFinalState(std::vector<int>& nums, int k, int multiplier) {
-        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+        std::vector<int> res(nums.size());
+        std::priority_queue<std::pair<int, int>,
+                            std::vector<std::pair<int, int>>,
+                            std::greater<std::pair<int, int>>> pq;
 
         for (int i = 0; i < nums.size(); i++) {
             pq.push({nums[i], i});
         }
 
-        while (k--) {
-            auto [n, i] = pq.top();
+        for (int i = 0; i < k; i++) {
+            auto [x, index] = pq.top();
             pq.pop();
-            pq.push({n * multiplier, i});
+            pq.push({x * multiplier, index});
         }
 
         while (!pq.empty()) {
-            auto [n, i] = pq.top();
+            auto [x, index] = pq.top();
             pq.pop();
-            nums[i] = n;
+            res[index] = x;
         }
 
-        return nums;
+        return res;
     }
 };
