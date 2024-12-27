2bf793e647023d8f1cbd9875c5466e93e3e296e3
wenbinteng <wenbinteng@163.com>
2024-12-27 23:08:39 +0800

    update

---
+++ 3159.h
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> occurrencesOfElement(std::vector<int>& nums, std::vector<int>& queries, int x) {
+        std::vector<int> res;
+        std::vector<int> pos;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == x) {
+                pos.push_back(i);
+            }
+        }
+
+        for (auto query : queries) {
+            if (query <= pos.size()) {
+                res.push_back(pos[query - 1]);
+            } else {
+                res.push_back(-1);
+            }
+        }
+
+        return res;
+    }
+};
