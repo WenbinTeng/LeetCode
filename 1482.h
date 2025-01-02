9f1f6b3aeab9364eb5f44563b73cf6dfb5b3750a
wenbinteng <wenbinteng@163.com>
2025-01-03 00:56:33 +0800

    update

--- 1482.h
+++ 1482.h
@@ -1,45 +1,51 @@
-#include <algorithm>
 #include <vector>
 
 class Solution {
 public:
-    int minDays(std::vector<int> &bloomDay, int m, int k) {
-        if (m * k > bloomDay.size())
+    int minDays(std::vector<int>& bloomDay, int m, int k) {
+        if (bloomDay.size() < (long long)m * k)
             return -1;
 
         int l = 0;
-        int r = *std::max_element(bloomDay.begin(), bloomDay.end());
+        int r = 1e+9;
+
+        auto checkInterval = [&](int days, int start) -> bool {
+            for (int i = 0; i < k; i++) {
+                if (start + i >= bloomDay.size())
+                    return false;
+                if (bloomDay[start + i] > days)
+                    return false;
+            }
+            return true;
+        };
+
+        auto checValid = [&](int days) -> bool {
+            int start = 0;
+            int r = m;
+            while (r && start < bloomDay.size()) {
+                if (checkInterval(days, start)) {
+                    start += k;
+                    r--;
+                } else {
+                    while (start < bloomDay.size() && bloomDay[start] <= days) {
+                        start++;
+                    }
+                    while (start < bloomDay.size() && bloomDay[start] > days) {
+                        start++;
+                    }
+                }
+            }
+            return r == 0;
+        };
 
         while (l < r) {
-            int mid = (l + r) / 2;
-
-            if (check(bloomDay, mid, m, k)) {
+            int mid = l + (r - l) / 2;
+            if (checValid(mid))
                 r = mid;
-            } else {
+            else
                 l = mid + 1;
-            }
         }
 
-        return l;
-    }
-
-private:
-    bool check(std::vector<int> &bloomDay, int days, int m, int k) {
-        int tm = 0;
-        int tk = 0;
-
-        for (const auto &day : bloomDay) {
-            if (day <= days) {
-                if (++tk == k) {
-                    if (++tm == m)
-                        return true;
-                    tk = 0;
-                }
-            } else {
-                tk = 0;
-            }
-        }
-
-        return false;
+        return r;
     }
 };
---
+++ 1483.h
@@ -0,0 +1,34 @@
+#include <vector>
+
+class TreeAncestor {
+public:
+    TreeAncestor(int n, std::vector<int>& parent) {
+        ancestor = std::vector<std::vector<int>>(n, std::vector<int>(16, -1));
+        for (int i = 0; i < parent.size(); i++)
+            ancestor[i][0] = parent[i];
+        for (int j = 1; j < 16; j++)
+            for (int i = 0; i < n; i++)
+                if (ancestor[i][j - 1] != -1)
+                    ancestor[i][j] = ancestor[ancestor[i][j - 1]][j - 1];
+    }
+    
+    int getKthAncestor(int node, int k) {
+        for (int j = 0; j < 16; j++) {
+            if ((k >> j) % 2 == 1) {
+                node = ancestor[node][j];
+                if (node == -1)
+                    return -1;
+            }
+        }
+        return node;
+    }
+
+private:
+    std::vector<std::vector<int>> ancestor;
+};
+
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
