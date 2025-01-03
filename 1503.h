6cd6813912aeb0fde889962f853ced804fb2d627
wenbinteng <wenbinteng@163.com>
2025-01-04 04:33:48 +0800

    update

---
+++ 1503.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int getLastMoment(int n, std::vector<int>& left, std::vector<int>& right) {
+        int res = 0;
+
+        for (auto pos : left) {
+            res = std::max(res, pos);
+        }
+        for (auto pos : right) {
+            res = std::max(res, n - pos);
+        }
+
+        return res;
+    }
+};
