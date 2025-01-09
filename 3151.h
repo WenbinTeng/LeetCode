32608c7be1f42cd8852953314c8a8e237533000d
wenbinteng <wenbinteng@163.com>
2025-01-09 16:11:01 +0800

    update

---
+++ 3151.h
@@ -0,0 +1,11 @@
+#include <vector>
+
+class Solution {
+public:
+    bool isArraySpecial(std::vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++)
+            if ((nums[i - 1] & 1) == (nums[i] & 1))
+                return false;
+        return true;
+    }
+};
