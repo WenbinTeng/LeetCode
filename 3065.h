2c1e0f56826e2d1807fb55f36f2855dcac445999
wenbinteng <wenbinteng@163.com>
2025-01-14 10:21:18 +0800

    update

---
+++ 3065.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+public:
+    int minOperations(std::vector<int>& nums, int k) {
+        int res = 0;
+
+        for (auto num : nums)
+            if (num < k)
+                res++;
+
+        return res;
+    }
+};
