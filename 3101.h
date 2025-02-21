8eceb4dae86f56451e1d15e4b8ef39c3f6747964
wenbinteng <wenbinteng@163.com>
2025-02-21 16:18:24 +0800

    update

---
+++ 3101.h
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution {
+public:
+    long long countAlternatingSubarrays(std::vector<int>& nums) {
+        long long res = 0;
+        int l = 0;
+        int r = 1;
+
+        while (r < nums.size()) {
+            if (nums[r] == nums[r - 1]) {
+                long long len = r - l;
+                long long sum = len * (len + 1) / 2;
+                res += sum;
+                l = r;
+            }
+            r++;
+        }
+
+        return res + ((long long)(r - l) * (r - l + 1) / 2);
+    }
+};
