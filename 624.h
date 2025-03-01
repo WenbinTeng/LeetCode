c5b5e1e9d5f47d6b7a76557a04f55d31341de754
wenbinteng <wenbinteng@163.com>
2025-03-01 10:30:07 +0800

    update

--- 624.h
+++ 624.h
@@ -3,16 +3,16 @@
 class Solution {
 public:
     int maxDistance(std::vector<std::vector<int>>& arrays) {
-        int ans = 0;
+        int res = 0;
         int min = INT_MAX / 2;
         int max = INT_MIN / 2;
 
         for (auto& a : arrays) {
-            ans = std::max({ans, a.back() - min, max - a.front()});
+            res = std::max({res, a.back() - min, max - a.front()});
             min = std::min(min, a.front());
             max = std::max(max, a.back());
         }
 
-        return ans;
+        return res;
     }
 };
