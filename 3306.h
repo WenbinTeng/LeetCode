e96031ef075f6094442784bfd7f0a8c60c7b61ef
wenbinteng <wenbinteng@163.com>
2025-03-13 16:53:13 +0800

    update

---
+++ 3306.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+
+class Solution {
+public:
+    long long countOfSubstrings(std::string word, int k) {
+        std::unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+
+        auto count = [&](int m) -> long long {
+            int n = word.size();
+            int consonants = 0;
+            long long res = 0;
+            std::unordered_map<char, int> occur;
+
+            for (int i = 0, j = 0; i < n; i++) {
+                while (j < n && (consonants < m || occur.size() < vowels.size())) {
+                    if (vowels.count(word[j])) {
+                        occur[word[j]]++;
+                    } else {
+                        consonants++;
+                    }
+                    j++;
+                }
+                if (consonants >= m && occur.size() == vowels.size()) {
+                    res += n - j + 1;
+                }
+                if (vowels.count(word[i])) {
+                    occur[word[i]]--;
+                    if (occur[word[i]] == 0) {
+                        occur.erase(word[i]);
+                    }
+                } else {
+                    consonants--;
+                }
+            }
+            
+            return res;
+        };
+
+        return count(k) - count(k + 1);
+    }
+};
