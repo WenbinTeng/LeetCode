9dcdf0343f8ae6f8e1237be6dc90ab130d97f270
wenbinteng <wenbinteng@163.com>
2025-09-11 16:31:47 +0800

    update

--- 560.cpp
+++ 560.cpp
@@ -1,8 +1,23 @@
 #include <vector>
+#include <unordered_map>
 
 class Solution {
 public:
     int subarraySum(std::vector<int>& nums, int k) {
-        
+        const int n = nums.size();
+        int res = 0;
+        int prevSum = 0;
+        std::unordered_map<int, int> dict;
+
+        dict[0] = 1;
+        for (int i = 0; i < n; i++) {
+            prevSum += nums[i];
+            if (dict.count(prevSum - k)) {
+                res += dict[prevSum - k];
+            }
+            dict[prevSum]++;
+        }
+
+        return res;
     }
 };
