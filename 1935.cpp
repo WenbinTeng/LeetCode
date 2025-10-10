4824a6630a979c9f6d5619dddbcf7f72b2db4280
wenbinteng <wenbinteng@163.com>
2025-10-10 16:51:26 +0800

    update

---
+++ 1935.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <sstream>
+#include <unordered_set>
+
+class Solution {
+public:
+    int canBeTypedWords(std::string text, std::string brokenLetters) {
+        int res = 0;
+        std::unordered_set<char> dict;
+        std::istringstream iss(text);
+        std::string token;
+
+        for (auto c : brokenLetters) {
+            dict.insert(c);
+        }
+
+        while (std::getline(iss, token, ' ')) {
+            bool valid = true;
+            for (auto c : token) {
+                if (dict.count(c)) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
